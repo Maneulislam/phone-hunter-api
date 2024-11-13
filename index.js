414f9bf6877e852176848fdb26a179c9ae747523
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2024-11-13 11:02:13 +0600

    Second commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText ,isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhone(phones);
+    displayPhone(phones, isShowAll);
 }
 
 
 
 
 
-const displayPhone = phones => {
+const displayPhone =( phones, isShowAll )=> {
     // console.log(phones);
 
     const phoneDiv = document.getElementById('phone-container');
@@ -19,7 +19,7 @@ const displayPhone = phones => {
 
 
     const showAllBtn = document.getElementById('Show-All-btn');
-    if(phones.length > 12){
+    if(phones.length > 12 && !isShowAll){
         showAllBtn.classList.remove('hidden');
     }
     else{
@@ -27,9 +27,15 @@ const displayPhone = phones => {
     }
 
 
-    phones = phones.slice(0,12);
+    console.log("Show All",isShowAll)
 
 
+    if(!isShowAll){
+        phones = phones.slice(0,12);
+
+    }
+
+    loadingSpinner(false);  
 
 
 
@@ -45,9 +51,9 @@ const displayPhone = phones => {
                     <div class="card-body">
                       <h2 class="card-title">Brand : ${phone.brand}</h2>
                       <h4>Phone Name : ${phone.phone_name}</h4>
-                      <p>${phone.slug}</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                      <p></p>
+                      <div class="card-actions justify-center">
+                        <button id="show-detail-btn" onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
                       </div>
                     </div>
         `;
@@ -56,16 +62,58 @@ const displayPhone = phones => {
         
     });
 
+    loadingSpinner(false);  
+
+
 }
 
 
 
-const handleSearch = () => {
+
+const handleShowDetail = async (id) => {
+    console.log("Click Show Details",id);
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    console.log(data);
+
+}
+
+
+
+
+
+
+const handleSearch = (isShowAll) => {
     const searchField = document.getElementById('searchInput')
     const searchText = searchField.value;
-    console.log(searchText)
-    loadPhone(searchText);
+    console.log(searchText) 
+    loadingSpinner(true);  
+    loadPhone(searchText,isShowAll);
 }
+
+
+
+
+const loadingSpinner = (isLoading) => {
+    const loadSpinnner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadSpinnner.classList.remove('hidden');
+    }
+    else{
+        loadSpinnner.classList.add('hidden');
+    }
+}
+
+
+
+
+const handleShowAll = (isShowAll) => {
+    handleSearch(true);
+
+}
+
+
 
 
 loadPhone();
